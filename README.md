ba18f1ae01b318b100d35fe46b5700eaaebe3d6d
wvwall <waltervelardo@gmail.com>
2021-03-25 14:21:40 +0100

    Html css e prima parte Vue

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 vue-todolist
+Descrizione
+Ricreare una todolist che permetta di aggiungere un todo e contrassegnarlo come completato mettendolo in fondo alla lista
+Bonus
+Modificare un elemento della lista, ricordandosi di aggiornare il title di quello giusto
---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+var app = new Vue({
+    el: "#root",
+    data: {
+        todos: [
+            {
+                title : "Fare esercizio Boolean"
+            },
+
+            {
+                title : "Avere scompensi emotivi"
+            },
+
+            {
+                title : "Aprire ticket"
+            },
+
+            {
+                title : "Pushare esercizio"
+            }
+        ]
+    }
+});
