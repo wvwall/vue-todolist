7b6eec08f0b17666cec3a282254a5348cf939469
wvwall <waltervelardo@gmail.com>
2021-03-25 15:38:39 +0100

    Computed

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,18 +25,38 @@ var app = new Vue({
             }
         ]
     },
+    computed : {
+        todosComputato : function () {
+
+            let todosDone = this.todos.filter((todo) => todo.status == "done");
+            let todosTodo = this.todos.filter((todo) => todo.status == "todo");
+            
+            return [...todosTodo, ...todosDone];
+        }
+        
+    },
     methods: {
         add : function () {
 
             if(this.inputTodo != "") {
                 let obj = {
-                    title : this.inputTodo
+                    title : this.inputTodo,
+                    status :"todo"
                 }
                 this.todos.push(obj);
-            }
-            this.inputTodo = ""
-
-            
+                this.inputTodo = "";
+                
+            } 
+        },
+        check: function (todo) {
+            let i = this.todos.indexOf(todo);
+            this.todos[i].status = "done";
+        },
+        remove: function(todo) {
+            let i = this.todo.indexOf(todo);
+            this.todos.splice(i, 1);
         }
     }
+    
+        
 });
