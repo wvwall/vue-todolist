f8b55431fd56555d8a304c1522d816c264eb0d92
wvwall <waltervelardo@gmail.com>
2021-03-25 14:48:34 +0100

    line-through when status is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,42 @@
+
 var app = new Vue({
     el: "#root",
     data: {
+        inputTodo: "",
         todos: [
             {
-                title : "Fare esercizio Boolean"
+                title : "Iniziare esercizio Boolean",
+                status : "done"
             },
 
             {
-                title : "Avere scompensi emotivi"
+                title : "Avere scompensi emotivi",
+                status : "done"
             },
 
             {
-                title : "Aprire ticket"
+                title : "Aprire ticket",
+                status : "todo"
             },
 
             {
-                title : "Pushare esercizio"
+                title : "Pushare esercizio",
+                status : "todo"
             }
         ]
+    },
+    methods: {
+        add : function () {
+
+            if(this.inputTodo != "") {
+                let obj = {
+                    title : this.inputTodo
+                }
+                this.todos.push(obj);
+            }
+            this.inputTodo = ""
+
+            
+        }
     }
 });
